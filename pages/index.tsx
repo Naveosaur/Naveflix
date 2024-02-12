987d31679862ec56b0c178b6f4e2986e79cc00d4
Evan Fadhilla <evfadhw@gmail.com>
2024-02-13 04:17:26 +0700

    feat - movie card

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,9 @@ import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import { useMemo } from "react";
+import useMovieList from "@/hooks/useMovieList";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -23,11 +26,15 @@ export async function getServerSideProps(context: NextPageContext) {
 
 export default function Home() {
   const { data: user } = userCurrentUser();
+  const { data: movies = [] } = useMovieList();
 
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className="pb-48">
+        <MovieList title="movie" data={movies} />
+      </div>
     </>
   );
 }
